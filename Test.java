6df93079221ef1ee13e25cd1e4d4997c378a7ba9
amruthpatil1998 <amruthpatl0051998@gmail.com>
2023-12-11 09:09:19 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+
+public static void main(String []args){
+
+int a=30;
+int b=20;
+int sum1=a+b;
+int sum2=a-b;
+System.out.println("sum of a and b is (a+b):"+sum1);
+System.out.println("sum of a and b is (a-b):"+sum2);
+}
+}
